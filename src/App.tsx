b241df78b659c1ee46b9e3ce3cc8517879f44abb
guardiangel <felix.sgq@gmail.com>
2023-09-10 19:34:35 -0400

    1. Delete unused files 2. Add comments

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Helloworld,Felixabc</div>;
+  return <div className="App">Helloworld</div>;
 }
 
 export default App;
