cc13789f3a07c180ba18aa19b8737157fd51243e
guardiangel <felix.sgq@gmail.com>
2023-09-11 00:09:51 -0400

    GetImageCode

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "./App.css";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
@@ -8,29 +8,9 @@ import { PersistGate } from "redux-persist/integration/react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import MainPage from "./pages/MainPage";
-import axios from "axios";
-import { error } from "console";
 
 function App() {
   const [theme, colorMode] = useMode();
-  const [imageCode, setImageCode] = useState();
-
-  const uuid = getUuid();
-
-  useEffect(() => {
-    axios
-      .get(`http://localhost:8020/login/getLoginImgCode/${uuid}`, {
-        headers: {
-          "Access-Control-Allow-Origin": true,
-        },
-      })
-      .then((res) => {
-        console.log(res.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, [uuid]);
 
   return (
     <Provider store={store}>
@@ -49,17 +29,5 @@ function App() {
     </Provider>
   );
 }
-const getUuid = (): string => {
-  let s: string[] = [];
-  let hexDigits = "0123456789abcdefghi";
-  for (let i = 0; i < 36; i++) {
-    s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
-  }
-  s[14] = "4"; // bits 12-15 of the time_hi_and_version field to 0010
-  s[19] = hexDigits.substr((Number(s[19]) & 0x3) | 0x8, 1); // bits 6-7 of the clock_seq_hi_and_reserved to 01
-  s[8] = s[13] = s[18] = s[23] = "-";
-  let uuid = s.join("");
-  return uuid;
-};
 
 export default App;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Avatar from "@mui/material/Avatar";
 import CssBaseline from "@mui/material/CssBaseline";
 import { Formik, Form, Field, ErrorMessage } from "formik";
@@ -8,12 +8,12 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
 import * as yup from "yup";
 import { useTheme } from "@mui/material";
 import { colorTokens } from "../theme";
 import { useDispatch } from "react-redux";
 import { setLoginState } from "../reducers/LoginReducerSlice";
+import axios from "axios";
 
 export default function Login() {
   const theme = useTheme();
@@ -21,6 +21,33 @@ export default function Login() {
   const navigate = useNavigate();
 
   const dispatch = useDispatch();
+
+  const [imageCode, setImageCode] = useState<string>("");
+
+  //fix eslint problem when using useEffect.
+  const fetchImageCode = useRef(() => {});
+
+  const uuid = getUuid();
+
+  fetchImageCode.current = () => {
+    axios
+      .get(`http://localhost:8020/login/getLoginImgCode/${uuid}`, {
+        headers: {
+          "Access-Control-Allow-Origin": true,
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+        setImageCode(res.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    fetchImageCode.current();
+  }, []);
 
   const handleSubmit = (data: any) => {
     console.log(data);
@@ -97,6 +124,12 @@ export default function Login() {
               }}
             />
 
+            <img
+              src={imageCode}
+              alt="imageCode"
+              onClick={fetchImageCode.current}
+            />
+
             <div>
               <button
                 type="submit"
@@ -121,6 +154,19 @@ export default function Login() {
     </Container>
   );
 }
+
+const getUuid = (): string => {
+  let s: string[] = [];
+  let hexDigits = "0123456789abcdefghi";
+  for (let i = 0; i < 36; i++) {
+    s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+  }
+  s[14] = "4"; // bits 12-15 of the time_hi_and_version field to 0010
+  s[19] = hexDigits.substr((Number(s[19]) & 0x3) | 0x8, 1); // bits 6-7 of the clock_seq_hi_and_reserved to 01
+  s[8] = s[13] = s[18] = s[23] = "-";
+  let uuid = s.join("");
+  return uuid;
+};
 
 const initialValues = {
   username: "",
